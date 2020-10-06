aa3449069aa2d16d048277964e153e6c14a646ea
lynever <lynever@naver.com>
2020-10-06 19:17:34 +0900

    remove

--- dashboard-server/modules/container.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl --unix-socket "/var/run/docker.sock" "http://plass/containers/$1/$2"
--- dashboard-server/modules/containers.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl --unix-socket "/var/run/docker.sock" "http://plass/containers/json?all=1&size=1" -v
